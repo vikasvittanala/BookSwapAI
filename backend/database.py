fef2b456a24f31441932788e3bdfa764e31254f7
Vikas Vittanala <sonu.vikas.v@gmail.com>
2026-05-30 23:09:01 +0800

    Added Supabase integration and initial script structure

---
+++ backend/database.py
@@ -0,0 +1,10 @@
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+supabase: Client = create_client(
+    os.getenv("SUPABASE_URL"),
+    os.getenv("SUPABASE_KEY")
+)
